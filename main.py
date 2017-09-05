dbad57a1c5b11c4e9edba343ca4c9cba90ee0a54
pyz3n <ale.d2001@gmail.com>
2017-09-05 16:17:16 +0200

    Drew things

--- main.py
+++ main.py
@@ -3,14 +3,21 @@
 
 import pygame as pg
 
-size = width, height = 600, 600
+size = width, height = 600, 500
 screen = pg.display.set_mode(size)
 done = False
 
 
-class Entity:
-    def __init__():
-        pass
+class Entity():
+    def __init__(self, pos, size, color):
+        self.pos = pos
+        self.size = size
+        self.color = color
+        self.rect = pg.Rect((0, 0), size)
+        self.rect.center = pos
+
+    def draw(self, surface):
+        pg.draw.rect(surface, self.color, self.rect)
 
 
 class Ant(Entity):
@@ -18,12 +25,21 @@ class Ant(Entity):
         Entity.__init__()
 
 
-def main():
+def event_loop():
     global done
     for event in pg.event.get():
         if event.type == pg.QUIT:
             done = True
 
 
+def main_loop():
+    asd = Entity((345, 290), (34, 42), (88,200,255))
+    asd.draw(screen)
+
+
 while not done:
-    main()
+    event_loop()
+    main_loop()
+    pg.display.flip()
+
+
